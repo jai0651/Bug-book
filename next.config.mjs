c7438649d65675c81e9e016f352e0e2234022b97
jai0651 <jaimauryatech@gmail.com>
2024-11-16 19:13:18 +0530

    added webpack in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,28 @@
+import 'dotenv/config';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    staleTimes: {
-      dynamic: 30,
-    },
-  },
-  serverExternalPackages: ["@node-rs/argon2"],
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "utfs.io",
-        pathname: `/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/*`,
+        pathname: `/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID || ''}/*`,
       },
     ],
   },
-  rewrites: () => {
-    return [
-      {
-        source: "/hashtag/:tag",
-        destination: "/search?q=%23:tag",
-      },
-    ];
+  rewrites: async () => [
+    {
+      source: "/hashtag/:tag",
+      destination: "/search?q=%23:tag",
+    },
+  ],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Externalize native modules
+      config.externals.push('@node-rs/argon2');
+    }
+    return config;
   },
 };
 
